a03fd939c09f21c2d53734f55d37805ecfe3e1e1
Sean Wills <rvb.seanwills@gmail.com>
2025-11-01 15:28:51 +0000

    new noncing

--- src/MiningCommunication.java
+++ src/MiningCommunication.java
@@ -1,11 +1,19 @@
 public class MiningCommunication {
     private String hash;
     private int leadingZeros;
+    private int attempts;
 
     public MiningCommunication(String hash) {
+        this.attempts = 0;
         this.hash = hash;
     }
+    public void incrementAttempts() {
+        this.attempts++;
+    }
 
+    public void printAttempts() {
+        System.out.println("Attempts: " + attempts);
+    }
     public String getHash() {
         return hash;
     }
--- src/MiningThread.java
+++ src/MiningThread.java
@@ -1,6 +1,9 @@
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MiningThread implements Runnable {
     private final String prevHash;
@@ -9,9 +12,6 @@ public class MiningThread implements Runnable {
     private final MiningCommunication mc;
     public final String threadName;
     private Thread t;
-    private Long nonce;
-    private Long endNonce;
-
 
     MiningThread(String prevHash, String psuedonym, OutputManager output, MiningCommunication mc, String threadName, long startNonce, long endNonce) {
         this.prevHash = prevHash;
@@ -19,42 +19,49 @@ public class MiningThread implements Runnable {
         this.output = output;
         this.mc = mc;
         this.threadName = threadName;
-        this.nonce = startNonce;
-        this.endNonce = endNonce;
     }
 
     public void run() {
-        while (mc.getLeadingZeros() < 50 || Objects.equals(nonce, endNonce)) {
-            MessageDigest sha;
-            try {
-                sha = MessageDigest.getInstance("SHA-256");
-            } catch (NoSuchAlgorithmException e) {
-                throw new RuntimeException(e);
+        // Setup Sha object
+        MessageDigest sha;
+        try {
+            sha = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        ByteBuffer nonceBuffer = ByteBuffer.allocate(Long.BYTES); // reuseable for all nonces
+        while (mc.getLeadingZeros() < 50) {
+            sha.reset(); // clear sha object
+            sha.update(mc.getHash().getBytes(StandardCharsets.UTF_8)); // add the bytes of previous hash
+            sha.update(psuedonym.getBytes(StandardCharsets.UTF_8)); // add the bytes of the psuedonym
+            long nonce = ThreadLocalRandom.current().nextLong();
+            nonceBuffer.clear(); // clear nonce buffer
+            sha.update(nonceBuffer.putLong(nonce).array()); // add random nonce and converts to bytes without using javas heavy string conversions. also need to find a way of caching the randoms
+            byte[] bin = sha.digest(); // gets the new hash as bytes
+            // Counting the zeros:
+            int zeroCount = 0;
+            for (byte b: bin) {
+                int unsigned = b & 0xFF; // applies a bitmask?
+                if (unsigned == 0) {
+                    zeroCount += 8;
+                } else {
+                    zeroCount += Integer.numberOfLeadingZeros(unsigned) - 24; // Only top 8 bits?
+                    break;
+                }
             }
-            String text = mc.getHash()+psuedonym+nonce;
-            sha.update(text.getBytes());
-            byte[] bin = sha.digest();
-            StringBuilder s = new StringBuilder();
-            for (byte b : bin) {
-                s.append(String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0')); // Converts Byte to Bits 0:
-            }
-            int zeros = 0;
-            for (char ch : s.toString().toCharArray()) {
-                if (ch != '0') break;
-                zeros++;
-            }
-            if (zeros > mc.getLeadingZeros()) {
-                output.block(text);
-                mc.setLeadingZeros(zeros);
+            //if (zeroCount > 20) System.out.println(zeroCount); // for testing
+            // Checks for a leading block, outputs and changes shared previous hash for other threads
+            if (zeroCount > mc.getLeadingZeros()) {
+                mc.setLeadingZeros(zeroCount);
                 StringBuilder hex = new StringBuilder();
                 for (byte b : bin) {
-                    hex.append(String.format("%02x", b));
+                    hex.append(String.format("%02x", b)); // convert byte to hex
                 }
+                output.block(mc.getHash()+psuedonym+nonce);
                 mc.setHash(hex.toString(), threadName);
             }
-            nonce++;
         }
-        System.out.println(threadName + " Finshed on " + nonce);
+        System.out.println(threadName + " Finshed ");
     }
 
     public void start () {
